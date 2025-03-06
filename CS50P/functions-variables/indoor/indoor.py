afddebdb938abc5747e5d8cb92a5011e6aaa85b8
Alokananda Y <plantsfortrees@gmail.com>
2025-03-06 03:43:22 +0000

    Starting from hello world

--- CS50P/functions-variables/indoor/indoor.py
+++
@@ -1,9 +0,0 @@
-# Prompt the user for input
-user_input = input()
-
-# Convert the input to lowercase
-lowercase_input = user_input.lower()
-
-# Output the lowercase input
-print(lowercase_input)
-
--- CS50P/mario-program/mario.py
+++
@@ -1,8 +0,0 @@
-def print_right_triangle(height):
-    for index in range(1, height + 1):
-        spaces = ' ' * (height - index)
-        stars = '*' * index
-        print(spaces + stars)
-
-# Example usage with height = 5
-print_right_triangle(5)
