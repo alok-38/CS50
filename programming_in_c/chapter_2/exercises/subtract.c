1a536b2d7fc6377a65f38b1d0644cc52926949c7
Alokananda Y <plantsfortrees@gmail.com>
2026-05-25 21:01:15 +0530

    Completing chapter 1

---
+++ programming_in_c/chapter_2/exercises/subtract.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int value_1 = 15;
+    int value_2 = 87;
+    printf("%i subtracted from %i gives %i\n", value_1, value_2, value_2 - value_1);
+
+    return 0;
+}
+
