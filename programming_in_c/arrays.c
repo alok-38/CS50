4fb69c3ac09430b00a38f6a6c15ea9fa8f42a65e
ThePrimeagen <the.primeagen@aol.com>
2026-07-12 07:32:05 +0530

    Arrays

---
+++ programming_in_c/arrays.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    int grades[3] = { 100, 90, 50 };
+    printf("The third grade in the array is %i\n", grades[2]);
+}
--- programming_in_c/variables.c
+++ programming_in_c/variables.c
@@ -5,4 +5,8 @@ int main(void) {
     int age_in_months = age * 12;
     int age_in_days = age_in_months * 30;
     int grades[4];
+    printf("%i\n", age);
+    printf("%i\n", age_in_months);
+    printf("%i\n", age_in_days);
+    printf("%i\n", grades);
 }
